887ee47546a3783b17a514b18454f0b1ea45fcf8
doublealchemist <119274717+doublealchemist@users.noreply.github.com>
2023-04-07 10:57:29 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.25.57:9000/',
+  serverUrl: 'http://172.31.8.167:9000/projects',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '18a4b9dba348014372f10d237a6bcfe4e2f9a543',
+	    'sonar.login': 'a346e04208c2df728dd5ed5f268c7e78344dd7ac',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
